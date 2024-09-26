09bbc44e87ad0d63e9b3c7aad3d14785add7d988
Arthurthekidboy <arthurcoutinho600@gmail.com>
2024-09-26 12:55:02 -0300

    Exercicio 1

---
+++ exercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Top 3 Consoles Favoritos</title>
+</head>
+<body>
+    <?php
+    echo "<h1>PlayStation 2</h1>";
+    echo "<img src='PS2.png'></img>";
+    echo "<h2>Super Nintendo</h2>";
+    echo "<img src='SNES.jpg'></img>";
+    echo "<h3>Sega Master System</h3>";
+    echo "<img src='SMS.png'></img>";
+    ?>
+</body>
+</html>
---
+++ exercicio2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print "<h1>URLs Absolutos</h1>";
+    print "<a href=https://forums.launchbox-app.com/>Launchbox Forums</a><br>";
+    print "<a href=https://emumovies.com/>EmuMovies</a>";
+
+    echo "<h2>URLs Relativos</h2>";
+    echo "<a href=oimeuchapa.html>Oi Meu Chapa</a><br>";
+    echo "<a href=tuninho.html>Tuninho</a>";
+    ?>
+</body>
+</html>
